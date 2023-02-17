dcb74e431d2c4855e1279d49571d9bdb31f7602e
asad1947 <asad1971@hotmail.com>
2023-02-17 13:19:46 +0000

    commit code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
 
 namespace dotnetcore
 {
@@ -6,7 +10,31 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string secretName = "mysecretmk";
+            string keyVaultName = "newkeymk";
+            string kvUri = "https://newkeymk.vault.azure.net";
+            SecretClientOptions options = new SecretClientOptions(){
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }                
+            };
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(),options);
+            KeyVaultSecret secret = client.GetSecret(secretName);
+            Console.WriteLine("GetSecret:"+secret.Value);
+            Console.Write("Enter Secret > ");
+
+            string secretValue = Console.ReadLine();
+            client.SetSecret(secretName, secretValue);
+            Console.Write("SetSecret:");
+            Console.Write("  Key: "+ secretName);
+            Console.Write("  Value: "+ secretValue);
+
+            Console.WriteLine("GetSecret: "+ secret.Value);
+            client.StartDeleteSecret(secretName);
+            Console.WriteLine("StartDeleteSecret:"+ keyVaultName);
         }
     }
 }
